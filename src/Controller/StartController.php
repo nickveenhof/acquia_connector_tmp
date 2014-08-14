8ab9655cbc67d73ff8036bd211a084849762210c
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2014-08-14 15:27:55 -0400

    Changed theme() calls to _theme() (https://www.drupal.org/node/2195739)

--- src/Controller/StartController.php
+++ src/Controller/StartController.php
@@ -28,7 +28,7 @@ class StartController extends ControllerBase {
       $path . '/css/acquia_connector.css',
     );
 
-    $banner = theme('image', array('uri' => $path . '/images/action.png'));
+    $banner = _theme('image', array('uri' => $path . '/images/action.png'));
     $banner = l($banner, 'admin/config/system/acquia-connector/setup', array('html' => TRUE, 'absolute' => TRUE));
 
     $output  = '<div class="an-wrapper">';
@@ -37,17 +37,17 @@ class StartController extends ControllerBase {
     $output .= '<div id="an-info-box">';
     $output .=   '<div class="cell with-arrow an-left">';
     $output .=     '<h2 class="cell-title"><i>' . $this->t('Answers you need') . '</i></h2>';
-    $output .=     '<a href="http://library.acquia.com/" target="_blank">' . theme('image', array('uri' => $path . '/images/icon-library.png')) . '</a>';
+    $output .=     '<a href="http://library.acquia.com/" target="_blank">' . _theme('image', array('uri' => $path . '/images/icon-library.png')) . '</a>';
     $output .=     '<p class="cell-p">' . $this->t("Tap the collective knowledge of Acquia’s technical support team & partners.") . '</p>';
     $output .=   '</div>';
     $output .=   '<div class="cell with-arrow an-center">';
     $output .=     '<h2 class="cell-title"><i>' . $this->t('Tools to extend your site') . '</i></h2>';
-    $output .=     '<a href="http://www.acquia.com/products-services/acquia-network/cloud-services" target="_blank">' . theme('image', array('uri' => $path . '/images/icon-tools.png')) . '</a>';
+    $output .=     '<a href="http://www.acquia.com/products-services/acquia-network/cloud-services" target="_blank">' . _theme('image', array('uri' => $path . '/images/icon-tools.png')) . '</a>';
     $output .=     '<p class="cell-p">' . $this->t('Enhance and extend your site with an array of <a href="@services" target="_blank">services</a> from Acquia & our partners.', array('@services' => 'http://www.acquia.com/products-services/acquia-network/cloud-services')) . '</p>';
     $output .=   '</div>';
     $output .=   '<div class="cell an-right">';
     $output .=     '<h2 class="cell-title"><i>' . $this->t('Support when you want it') . '</i></h2>';
-    $output .=     '<a href="http://www.acquia.com/drupal-support" target="_blank">' . theme('image', array('uri' => $path . '/images/icon-support.png')) . '</a>';
+    $output .=     '<a href="http://www.acquia.com/drupal-support" target="_blank">' . _theme('image', array('uri' => $path . '/images/icon-support.png')) . '</a>';
     $output .=     '<p class="cell-p">' . $this->t("Experienced Drupalists are available to support you whenever you need it.") . '</p>';
     $output .=   '</div>';
     $output .=   '<div class="an-pg-banner">';
